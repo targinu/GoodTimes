9408f4991ddb04e470478c9f6b2228962151f738
targinu <targino26@outlook.com>
2023-03-28 22:37:45 -0300

    commit inicial

--- app/src/main/java/com/example/atividaden1/LoginActivity.kt
+++ app/src/main/java/com/example/atividaden1/LoginActivity.kt
@@ -30,6 +30,7 @@ class LoginActivity : AppCompatActivity() {
             val email = binding.username.text.toString()
             val pass = binding.password.text.toString()
 
+
             if (email.isNotEmpty() && pass.isNotEmpty()){
                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
                         if (it.isSuccessful){
